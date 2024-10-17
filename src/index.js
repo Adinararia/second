1e1f8a3d46eb51565bceb57fa33ffe388f15c86a
adinar <adinaria223311@gmail.com>
2024-10-18 00:51:41 +0300

    add route and doenv v2

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import http from 'http'
+import dotenv from 'dotenv'
+
+dotenv.config()
+const projectName = process.env.PROJECT_NAME
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' })
-    res.write('22')
-    res.end('asdfasdf')
+
+    if (req.url === '/test' && req.method === 'GET') {
+        res.write('test\n')
+    } else {
+        res.write('start page\n')
+    }
+    res.end('finish')
 })
 
 server.listen(3000, '127.0.0.1', () => {
     console.log('Listening on 127.0.0.1:3000')
+    console.log(projectName)
 })
