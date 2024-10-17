fafb5397e8635b78160c734f173b4d88b92658c2
adinar <adinaria223311@gmail.com>
2024-10-17 23:48:50 +0300

    add new modules and start server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('hello world')
+import http from 'http'
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' })
+    res.write('22')
+    res.end('asdfasdf')
+})
+
+server.listen(3000, '127.0.0.1', () => {
+    console.log('Listening on 127.0.0.1:3000')
+})
